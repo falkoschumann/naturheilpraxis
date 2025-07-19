b90fb5bd665c26113f077620d7dba1236b753180
Falko Schumann <falko.schumann@muspellheim.de>
2025-07-19 10:22:23 +0200

    feat: use virtual table

--- src/renderer/ui/patientenkartei.tsx
+++ src/renderer/ui/patientenkartei.tsx
@@ -1,31 +1,66 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
-import { useEffect, useState } from "react";
+import { createColumnHelper, flexRender, getCoreRowModel, type Table, useReactTable } from "@tanstack/react-table";
+import { type RefObject, useEffect, useRef, useState } from "react";
 import { NavLink, useNavigate } from "react-router";
 
 import type { Patient } from "../../main/domain/naturheilpraxis";
 import { PATIENT_AUFNEHMEN_PAGE, PATIENTENKARTEI_PAGE } from "./pages";
+import { useVirtualizer } from "@tanstack/react-virtual";
+
+const columnHelper = createColumnHelper<Patient>();
+const columns = [
+  columnHelper.accessor("nummer", { header: "#", size: 80 }),
+  columnHelper.accessor("anrede", { header: "Anrede", size: 80 }),
+  columnHelper.accessor("nachname", { header: "Nachname" }),
+  columnHelper.accessor("vorname", { header: "Vorname" }),
+  columnHelper.accessor("geburtsdatum", {
+    header: "Geburtsdatum",
+    cell: (info) => new Date(info.getValue()).toLocaleDateString(undefined, { dateStyle: "medium" }),
+  }),
+  columnHelper.accessor("strasse", { header: "Straße" }),
+  columnHelper.accessor("postleitzahl", { header: "PLZ", size: 80 }),
+  columnHelper.accessor("wohnort", { header: "Wohnort" }),
+  columnHelper.accessor("telefon", {
+    header: "Telefon",
+    cell: (info) => (
+      <a href={`tel:${info.getValue()}`} onClick={(event) => event.stopPropagation()}>
+        {info.getValue()}
+      </a>
+    ),
+  }),
+  columnHelper.accessor("mobil", {
+    header: "Mobil",
+    cell: (info) => (
+      <a href={`tel:${info.getValue()}`} onClick={(event) => event.stopPropagation()}>
+        {info.getValue()}
+      </a>
+    ),
+  }),
+  columnHelper.accessor("eMail", {
+    header: "E-Mail",
+    size: 250,
+    cell: (info) => (
+      <a href={`mailto:${info.getValue()}`} onClick={(event) => event.stopPropagation()}>
+        {info.getValue()}
+      </a>
+    ),
+  }),
+];
 
 export default function Patientenkartei() {
-  const [patienten, setPatienten] = useState<Patient[]>();
-  const navigate = useNavigate();
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+  const [data, setData] = useState<Patient[]>([]);
+  const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() });
 
   useEffect(() => {
     async function queryPatientenkartei() {
       const result = await window.naturheilpraxis.patientenkartei({});
-      setPatienten(result.patienten);
+      setData(result.patienten);
     }
 
     void queryPatientenkartei();
   }, []);
-
-  function handlePatientClick(nummer: number) {
-    navigate(`${PATIENTENKARTEI_PAGE}/${nummer}`);
-  }
-
-  function handleStopPropagation(event: React.MouseEvent<HTMLElement>) {
-    event.stopPropagation();
-  }
 
   return (
     <main className="container-fluid my-4">
@@ -40,86 +75,24 @@ export default function Patientenkartei() {
           </form>
         </div>
       </div>
-      <div style={{ height: "calc(100vh - 13.5rem)", overflowY: "auto" }}>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col" className="sticky-top">
-                #
-              </th>
-              <th scope="col" className="sticky-top">
-                Anrede
-              </th>
-              <th scope="col" className="sticky-top">
-                Nachname
-              </th>
-              <th scope="col" className="sticky-top">
-                Vorname
-              </th>
-              <th scope="col" className="sticky-top">
-                Geburtsdatum
-              </th>
-              <th scope="col" className="sticky-top">
-                Straße
-              </th>
-              <th scope="col" className="sticky-top">
-                PLZ
-              </th>
-              <th scope="col" className="sticky-top">
-                Wohnort
-              </th>
-              <th scope="col" className="sticky-top">
-                Telefon
-              </th>
-              <th scope="col" className="sticky-top">
-                Mobil
-              </th>
-              <th scope="col" className="sticky-top">
-                E-Mail
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {!patienten && (
-              <tr>
-                <td colSpan={11}>
-                  <div className="d-flex justify-content-center align-items-center">
-                    <div className="spinner-border text-primary" role="status">
-                      <span className="visually-hidden">Loading...</span>
-                    </div>
-                    <div className="ms-2">Lade Patienten ...</div>
-                  </div>
-                </td>
-              </tr>
-            )}
-            {patienten?.map((patient: Patient) => (
-              <tr id={String(patient.nummer)} key={patient.nummer} onClick={() => handlePatientClick(patient.nummer)}>
-                <th scope="row">{patient.nummer}</th>
-                <td>{patient.anrede}</td>
-                <td>{patient.nachname}</td>
-                <td>{patient.vorname}</td>
-                <td>{new Date(patient.geburtsdatum).toLocaleDateString(undefined, { dateStyle: "medium" })}</td>
-                <td>{patient.strasse}</td>
-                <td>{patient.postleitzahl}</td>
-                <td>{patient.wohnort}</td>
-                <td>
-                  <a href={`tel:${patient.telefon}`} onClick={handleStopPropagation}>
-                    {patient.telefon}
-                  </a>
-                </td>
-                <td>
-                  <a href={`tel:${patient.mobil}`} onClick={handleStopPropagation}>
-                    {patient.mobil}
-                  </a>
-                </td>
-                <td>
-                  <a href={`mailto:${patient.eMail}`} onClick={handleStopPropagation}>
-                    {patient.eMail}
-                  </a>
-                </td>
+      <div
+        ref={tableContainerRef}
+        className="position-relative overflow-auto"
+        style={{ height: "calc(100vh - 13.5rem)" }}
+      >
+        <table className="d-grid table table-hover">
+          <thead className="d-grid sticky-top">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id} className="d-flex w-100">
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} scope="col" className="d-flex" style={{ width: header.getSize() }}>
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </th>
+                ))}
               </tr>
             ))}
-          </tbody>
+          </thead>
+          <TableBody table={table} tableContainerRef={tableContainerRef} />
         </table>
       </div>
       <div className="btn-toolbar mt-3" role="toolbar" aria-label="Aktionen für Patienten">
@@ -128,5 +101,55 @@ export default function Patientenkartei() {
         </NavLink>
       </div>
     </main>
+  );
+}
+
+function TableBody({
+  table,
+  tableContainerRef,
+}: {
+  table: Table<Patient>;
+  tableContainerRef: RefObject<HTMLDivElement | null>;
+}) {
+  const { rows } = table.getRowModel();
+  const virtualizer = useVirtualizer<HTMLDivElement, HTMLTableRowElement>({
+    count: rows.length,
+    estimateSize: () => 41,
+    getScrollElement: () => tableContainerRef.current,
+    measureElement:
+      typeof window !== "undefined" && navigator.userAgent.indexOf("Firefox") === -1
+        ? (element) => element?.getBoundingClientRect().height
+        : undefined,
+    overscan: 5,
+  });
+
+  const navigate = useNavigate();
+
+  function handlePatientClick(nummer: number) {
+    navigate(`${PATIENTENKARTEI_PAGE}/${nummer}`);
+  }
+
+  return (
+    <tbody className="d-grid position-relative" style={{ height: `${virtualizer.getTotalSize()}px` }}>
+      {virtualizer.getVirtualItems().map((virtualRow) => {
+        const row = rows[virtualRow.index];
+        return (
+          <tr
+            key={row.id}
+            data-index={virtualRow.index}
+            ref={(node) => virtualizer.measureElement(node)}
+            className="d-flex position-absolute w-100"
+            style={{ transform: `translateY(${virtualRow.start}px)` }}
+            onClick={() => handlePatientClick(row.getValue("nummer"))}
+          >
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id} className="d-flex" style={{ width: cell.column.getSize() }}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        );
+      })}
+    </tbody>
   );
 }
