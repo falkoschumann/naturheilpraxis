78a5f6ef12ac0cd10cdf0ff1d73b24866b0c2c59
Falko Schumann <falko.schumann@muspellheim.de>
2025-08-10 22:14:28 +0200

    build: add eslint react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,26 @@
 
 import js from "@eslint/js";
 import headers from "eslint-plugin-headers";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import ts from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["coverage", "dist"] },
+export default ts.config(
+  { ignores: ["coverage", "dist", "out"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [js.configs.recommended, ...ts.configs.recommended],
+    files: ["**/*.{cjs,mjs,js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      headers,
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      headers,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
