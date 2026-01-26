e1f37001efce81f21025f6a7a4aa99f3ff56f158
Falko Schumann <falko.schumann@muspellheim.de>
2026-01-26 20:12:26 +0100

    docs: clean up comments

--- src/main/index.ts
+++ src/main/index.ts
@@ -73,9 +73,9 @@ app.on("window-all-closed", () => {
   }
 });
 
+// On OS X it's common to re-create a window in the app when the
+// dock icon is clicked and there are no other windows open.
 app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
