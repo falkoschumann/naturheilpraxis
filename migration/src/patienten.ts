4b6767695b1b1a63275613fac1b762e339bb2a1e
Falko Schumann <falko.schumann@muspellheim.de>
2026-03-23 22:44:13 +0100

    refactor: extract functions

--- migration/src/patienten.ts
+++ migration/src/patienten.ts
@@ -8,7 +8,9 @@ import { Temporal } from "@js-temporal/polyfill";
 const MIGRATIONSFEHLER = "Migrationsfehler";
 
 export function erstellePatienten(customers: CustomerDto[]) {
-  return customers.map((customer) => erstellePatient(customer));
+  return customers.map((customer) => {
+    return erstellePatient(customer);
+  });
 }
 
 function erstellePatient(customer: CustomerDto) {
@@ -19,49 +21,35 @@ function erstellePatient(customer: CustomerDto) {
   // TODO Sichere Leerstring-Werte als undefined?
 
   let notizen = erstelleNotizen(customer);
-  const schluesselworte = erstelleSchluesselworte(customer.handlings);
-
-  if (customer.dayOfBirth != null) {
-    if (customer.dayOfBirth.trim() === "") {
-      customer.dayOfBirth = undefined;
-    } else {
-      try {
-        Temporal.PlainDate.from(customer.dayOfBirth);
-      } catch {
-        console.warn(
-          `  Ungültiges Geburtsdatum für ${customer.surname}, ${customer.forename} (${customer.id}) gefunden: ${customer.dayOfBirth}`,
-        );
-        customer.dayOfBirth = undefined;
-        schluesselworte.push(MIGRATIONSFEHLER, "Geburtsdatum");
-        if (notizen.length > 0) {
-          notizen += "\n\n";
-        }
-        notizen += `Ungültiges Geburtsdatum: ${customer.dayOfBirth}`;
-      }
-    }
-  }
+  let schluesselworte = erstelleSchluesselworte(customer);
+  const geburtsdatum = erstelleGeburtsdatum(customer, {
+    onError: (neueSchluesselworte, ergaenzendeNotizen) => {
+      schluesselworte = [...schluesselworte, ...neueSchluesselworte];
+      notizen = ergaenzeNotizen(notizen, ergaenzendeNotizen);
+    },
+  });
 
   return Patient.create({
     nummer: customer.id,
-    nachname: customer.surname,
-    vorname: customer.forename,
-    geburtsdatum: customer.dayOfBirth,
+    nachname: normalisiereString(customer.surname),
+    vorname: normalisiereString(customer.forename),
+    geburtsdatum,
     annahmejahr: customer.acceptance,
-    praxis: customer.agency,
-    anrede: customer.title,
-    strasse: customer.street,
-    wohnort: customer.city,
-    postleitzahl: customer.postalCode,
-    staat: customer.country,
-    staatsangehoerigkeit: customer.citizenship,
-    titel: customer.academicTitle,
-    beruf: customer.occupation,
-    telefon: customer.callNumber,
-    mobil: customer.mobilePhone,
-    eMail: customer.email,
-    familienstand: customer.familyStatus,
-    partner: customer.partnerFrom,
-    eltern: customer.childFrom,
+    praxis: normalisiereString(customer.agency),
+    anrede: normalisiereString(customer.title),
+    strasse: normalisiereString(customer.street),
+    wohnort: normalisiereString(customer.city),
+    postleitzahl: normalisiereString(customer.postalCode),
+    staat: normalisiereString(customer.country),
+    staatsangehoerigkeit: normalisiereString(customer.citizenship),
+    titel: normalisiereString(customer.academicTitle),
+    beruf: normalisiereString(customer.occupation),
+    telefon: normalisiereString(customer.callNumber),
+    mobil: normalisiereString(customer.mobilePhone),
+    eMail: normalisiereString(customer.email),
+    familienstand: normalisiereString(customer.familyStatus),
+    partner: normalisiereString(customer.partnerFrom),
+    eltern: normalisiereString(customer.childFrom),
     // TODO kinder: customer.xxx,
     // TODO geschwister: customer.xxx,
     notizen,
@@ -70,13 +58,61 @@ function erstellePatient(customer: CustomerDto) {
 }
 
 function erstelleNotizen(customer: CustomerDto) {
-  return customer.memorandum?.trim() ?? "";
+  return normalisiereString(customer.memorandum) ?? "";
 }
 
-function erstelleSchluesselworte(value?: string) {
-  if (value == null) {
+function erstelleSchluesselworte(customer: CustomerDto) {
+  const handlings = normalisiereString(customer.handlings);
+  if (handlings == null) {
     return [];
   }
 
-  return value.split(",").map((s) => String(s).trim());
+  return handlings.split(",").map((s) => String(s).trim());
+}
+
+function erstelleGeburtsdatum(
+  customer: CustomerDto,
+  {
+    onError,
+  }: {
+    onError: (
+      neueSchluesselworte: string[],
+      ergaenzendeNotizen: string,
+    ) => void;
+  },
+) {
+  const dayOfBirth = normalisiereString(customer.dayOfBirth);
+  if (dayOfBirth == null) {
+    return;
+  }
+
+  try {
+    return Temporal.PlainDate.from(dayOfBirth);
+  } catch {
+    console.warn(
+      `  Ungültiges Geburtsdatum für ${customer.surname}, ${customer.forename} (${customer.id}) gefunden: ${customer.dayOfBirth}`,
+    );
+    onError(
+      [MIGRATIONSFEHLER, "Geburtsdatum"],
+      `Ungültiges Geburtsdatum: ${customer.dayOfBirth}`,
+    );
+    return;
+  }
+}
+
+function ergaenzeNotizen(notizen: string, ergaenzendeNotizen: string) {
+  if (notizen.length > 0) {
+    notizen += "\n\n";
+  }
+  notizen += ergaenzendeNotizen;
+  return notizen;
+}
+
+function normalisiereString(wert?: string): string | undefined {
+  wert = wert && String(wert).trim();
+  if (wert == null || wert.length === 0) {
+    return;
+  }
+
+  return wert.trim();
 }
