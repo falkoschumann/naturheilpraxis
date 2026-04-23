ec87e29133b31c34197ef2e20b48485c89dc362a
Falko Schumann <falko.schumann@muspellheim.de>
2026-04-23 20:41:00 +0200

    build: use code splitting

--- src/renderer/ui/index.tsx
+++ src/renderer/ui/index.tsx
@@ -1,17 +1,19 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
 import "bootstrap";
+import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import "./assets/bootstrap.scss";
 import "./assets/fontawesome.css";
 import { PATIENTENKARTEI_PAGE, PATIENTENKARTEIKARTE_PAGE, START_PAGE } from "./components/pages";
-import PatientenkarteiPage from "./pages/patientenkartei";
-import PatientenkarteikartePage from "./pages/patientenkarteikarte";
-import StartseitePage from "./pages/startseite";
-import LeistungenComponent from "./pages/patientenkarteikarte/leistungen";
-import PatientComponent from "./pages/patientenkarteikarte/patient";
-import RechnungenComponent from "./pages/patientenkarteikarte/rechnungen";
+
+const PatientenkarteiPage = lazy(() => import("./pages/patientenkartei"));
+const PatientenkarteikartePage = lazy(() => import("./pages/patientenkarteikarte"));
+const StartseitePage = lazy(() => import("./pages/startseite"));
+const LeistungenComponent = lazy(() => import("./pages/patientenkarteikarte/leistungen"));
+const PatientComponent = lazy(() => import("./pages/patientenkarteikarte/patient"));
+const RechnungenComponent = lazy(() => import("./pages/patientenkarteikarte/rechnungen"));
 
 export default function App() {
   return (
