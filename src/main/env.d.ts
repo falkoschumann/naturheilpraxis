9ce9e0ac91cdff4974467adc180fe2b5e1624099
Falko Schumann <falko.schumann@muspellheim.de>
2026-04-28 20:31:08 +0200

    chore: workaround for header issue

--- src/main/env.d.ts
+++ src/main/env.d.ts
@@ -1,3 +1,8 @@
 // Copyright (c) 2026 Falko Schumann. All rights reserved. MIT license.
 
 /// <reference types="electron-vite/node" />
+
+declare module "dummy" {
+  // WORKAROUND No header found - headers/header-format
+  //   when using eslint-plugin-headers@1.3.4
+}
--- src/renderer/env.d.ts
+++ src/renderer/env.d.ts
@@ -1,3 +1,8 @@
 // Copyright (c) 2025 Falko Schumann. All rights reserved. MIT license.
 
 /// <reference types="vite/client" />
+
+declare module "dummy" {
+  // WORKAROUND No header found - headers/header-format
+  //   when using eslint-plugin-headers@1.3.4
+}
